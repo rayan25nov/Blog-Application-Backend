1c5ff1ed21ebc05ca65e5b3122cbe0260b6ead89
Rayan Ahmad <rayan25nov@gmail.com>
2024-12-02 21:27:27 +0530

    Removed Authentication for get request

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -26,23 +26,23 @@ import {
 import uploadImage from "../middleware/cloudinaryMiddleware.js";
 
 // Post Related Routes
-router.get("/", requireAuth, getAllPosts);
+router.get("/", getAllPosts);
 router.post("/create", requireAuth, createPost);
 router.get("/:id", getPostById);
 router.put("/:id", requireAuth, updatePost);
 router.delete("/:id", requireAuth, deletePost);
 
-// Post by user Id 
+// Post by user Id
 router.get("/user/:id", getPostsByUserId);
 
 // All the likes related Routes
 router.put("/like/:id", requireAuth, likePost);
 router.put("/unlike/:id", requireAuth, unlikePost);
-router.get("/likes/:id", requireAuth, getAllLikes);
+router.get("/likes/:id", getAllLikes);
 
 // All the comments related Routes
 router.put("/comment/:postId", requireAuth, commentPost);
 router.delete("/comment/:postId/:commentId", requireAuth, deleteComment);
-router.get("/comment/:postId", requireAuth, getAllCommentsForSpecificPost);
+router.get("/comment/:postId", getAllCommentsForSpecificPost);
 
 export default router;
